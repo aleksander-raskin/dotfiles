a4ed20859a735dcb5c4890cd388923c5237c3e3d
Aleksander Raskin <aleksander.raskin@contentsquare.com>
2020-01-29 16:37:48 +0200

    adding symbolic link creation script

---
+++ symlink.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ln -sf ~/dotfiles/.zshenv ~/.zshenv
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.zsh_functions ~/.zsh_functions
+ln -sf ~/dotfiles/.zshalias ~/.zshalias
+ln -sf ~/dotfiles/.zshplugins ~/.zshplugins
+ln -sf ~/dotfiles/.zshpy ~/.zshpy
